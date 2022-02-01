c7313f35bf397ffa6963a5319c5f116708087896
Conticello <ibo.j.conticello@outlook.com>
2022-02-01 00:02:57 -0300

    Minor fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,18 +2,18 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container pe-0">
-                <a class="navbar-brand ms-0 ps-0" href="">WARE <em class="brand-tech">TECH</em></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item px-4 nav-link">HOME</li>
-                        <li class="nav-item px-4 nav-link">PC GAMER</li>
-                        <li class="nav-item px-4 nav-link">GPU</li>
-                        <li class="nav-item px-4 nav-link">CPU</li>
-                        <li class="nav-item px-4 nav-link">RAM</li>
-                        <li class="nav-item px-4 nav-link">TOWER</li>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container pe-0">
+                <a className="navbar-brand ms-0 ps-0" href="">WARE <em className="brand-tech">TECH</em></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item px-4 nav-link">HOME</li>
+                        <li className="nav-item px-4 nav-link">PC GAMER</li>
+                        <li className="nav-item px-4 nav-link">GPU</li>
+                        <li className="nav-item px-4 nav-link">CPU</li>
+                        <li className="nav-item px-4 nav-link">RAM</li>
+                        <li className="nav-item px-4 nav-link">TOWER</li>
                     </ul>
                 </div>
             </div>
