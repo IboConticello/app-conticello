296ed7953ed42bacf541de597c23ce146a5b4522
Conticello <ibo.j.conticello@outlook.com>
2022-03-26 15:07:32 -0300

    Applied env file

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeiXZbyrsosVy-OaKKWX62m4a4WLt077s",
-    authDomain: "coderhouse-react-24925.firebaseapp.com",
-    projectId: "coderhouse-react-24925",
-    storageBucket: "coderhouse-react-24925.appspot.com",
-    messagingSenderId: "800551992622",
-    appId: "1:800551992622:web:daa51bcff8b44f4e7e0ebf"
+    apiKey: process.env.REACT_APP_ApiKey,
+    authDomain: process.env.REACT_APP_AuthDomain,
+    projectId: process.env.REACT_APP_ProjectId,
+    storageBucket: process.env.REACT_APP_StorageBucket,
+    messagingSenderId: process.env.REACT_APP_MessagingSenderId,
+    appId: process.env.REACT_APP_AppId
 };
 
 // Initialize Firebase
